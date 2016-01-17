eb9716b06ac7531834ee2b7323974d4827c9f60d
John Wass <jwass3@gmail.com>
2016-01-17 12:07:02 -0500

    [dont merge]

--- src/test/scala/com/rxthings/inject/SpiSpec.scala
+++ src/test/scala/com/rxthings/inject/SpiSpec.scala
@@ -6,10 +6,10 @@ import org.scalatest.Matchers
 
 class SpiSpec extends InjectSpec with Matchers {
 
-    "SPI injection" should {
-        injectTest("load modules defined in service files", cfg = cfg(spiMode)) { implicit sys =>
-            injector.instance[String] shouldBe SpiTests.stringVal
-            injector.instance[Int] shouldBe SpiTests.intVal
-        }
-    }
+//    "SPI injection" should {
+//        injectTest("load modules defined in service files", cfg = cfg(spiMode)) { implicit sys =>
+//            injector.instance[String] shouldBe SpiTests.stringVal
+//            injector.instance[Int] shouldBe SpiTests.intVal
+//        }
+//    }
 }
